40b45b6e3422aab8473520b88106a9857b39a26f
Hanjae Lee <hanjae.lee@icloud.com>
2015-04-08 19:05:13 +0900

    iron router is removed because code is incomplete

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 extern crate iron;
-extern crate router;
 
-use iron::{Iron, Request, Response, IronResult};
+use iron::prelude::*;
 use iron::status;
-use router::{Router};
 
 fn main() {
-    let mut router = Router::new();
-    router.get("/", handler);
-    router.get("/:query", handler);
-
-    Iron::new(router).http("localhost:3000").unwrap();
-
-    fn handler(req: &mut Request) -> IronResult<Response> {
-        let ref query = req.extensions.get::<Router>()
-            .unwrap().find("query").unwrap_or("/");
-        Ok(Response::with((status::Ok, *query)))
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hello World!")))
     }
+
+    Iron::new(hello_world).http("localhost:3000").unwrap();
+    println!("On 3000");
 }
