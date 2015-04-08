ce28e8ae3e6e977165a3e1a9ef2cec6c688d034f
Hanjae Lee <hanjae.lee@icloud.com>
2015-04-08 18:51:10 +0900

    Containers tab is added in index.html

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 extern crate iron;
+extern crate router;
 
-use iron::prelude::*;
+use iron::{Iron, Request, Response, IronResult};
 use iron::status;
+use router::{Router};
 
 fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }
+    let mut router = Router::new();
+    router.get("/", handler);
+    router.get("/:query", handler);
 
-    Iron::new(hello_world).http("localhost:3000").unwrap();
-    println!("On 3000");
+    Iron::new(router).http("localhost:3000").unwrap();
+
+    fn handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>()
+            .unwrap().find("query").unwrap_or("/");
+        Ok(Response::with((status::Ok, *query)))
+    }
 }
